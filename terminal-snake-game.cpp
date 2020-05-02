f2019a227ee9aacff3bba77ba507107d0519e050
mikecullen76 <mikecullen76@gmail.com>
2020-05-02 14:50:41 +0200

    1st commit

--- terminal-snake-game.cpp
+++ terminal-snake-game.cpp
@@ -10,64 +10,58 @@
 
 using namespace std;
 
-char getch()
-{
-  char buf = 0;
-  struct termios old = {0};
-  fflush(stdout);
-  if (tcgetattr(0, &old) < 0)
-    perror("tcsetattr()");
-  old.c_lflag &= ~ICANON;
-  old.c_lflag &= ~ECHO;
-  old.c_cc[VMIN] = 1;
-  old.c_cc[VTIME] = 0;
-  if (tcsetattr(0, TCSANOW, &old) < 0)
-    perror("tcsetattr ICANON");
-  if (read(0, &buf, 1) < 0)
-    perror("read()");
-  old.c_lflag |= ICANON;
-  old.c_lflag |= ECHO;
-  if (tcsetattr(0, TCSADRAIN, &old) < 0)
-    perror ("tcsetattr ~ICANON");
+char getch(){
 
-  return buf;
+    char buf=0;
+    struct termios old={0};
+    fflush(stdout);
+    if(tcgetattr(0, &old)<0)
+        perror("tcsetattr()");
+    old.c_lflag&=~ICANON;
+    old.c_lflag&=~ECHO;
+    old.c_cc[VMIN]=1;
+    old.c_cc[VTIME]=0;
+    if(tcsetattr(0, TCSANOW, &old)<0)
+        perror("tcsetattr ICANON");
+    if(read(0,&buf,1)<0)
+        perror("read()");
+    old.c_lflag|=ICANON;
+    old.c_lflag|=ECHO;
+    if(tcsetattr(0, TCSADRAIN, &old)<0)
+        perror ("tcsetattr ~ICANON");
+    return buf;
 }
-
-//keyboard hit
 
 int kbhit(void)
 {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-  ch = getchar();
+    ch = getchar();
 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-  if (ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
-
-//for color
 
 #ifndef _COLORS_
 #define _COLORS_
 
-/* FOREGROUND */
 #define RST  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -91,180 +85,159 @@ int kbhit(void)
 #endif  /* _COLORS_ */
 
 bool gameover;
-const int width = 40;
-const int height = 40;
-int x, y, fruitx, fruity, score;
-int tailx[100], taily[100];
+const int width=40;
+const int height=40;
+int x,y,fruitx,fruity,score;
+int tailx[100],taily[100];
 int ntail;
 
-enum edirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum edirection{STOP=0,LEFT,RIGHT,UP,DOWN};
 
 edirection dir;
 
 unsigned int microseconds = 10000;
 
+
 void setup()
 {
-  gameover = false;
-  dir = STOP;
-  x = width / 2;
-  y = height / 2;
-  fruitx = rand() % width;
-  fruity = rand() % height;
-  score = 0;
+    gameover=false;
+    dir = STOP;
+    x = width/2;
+    y = height/2;
+    fruitx=rand() % width;
+    fruity=rand() % height;
+    score = 0;
 }
+
+
 
 void draw()
 {
-  system("clear");
-  for (int i = 0; i < width + 2; i++)
-  {
-    cout << FGRN("#");
-  }
-  cout << endl;
-
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
+    system("clear");
+    for(int i =0; i<width+2;i++)
+        cout << FGRN("#");
+    cout<<endl;
+    for(int i=0;i<height;i++)
     {
-      if (j == 0)
-      {
-        cout << FBLU("#");
-      }
-      if (i == y && j == x)
-      {
-        cout << FGRN("0");
-      }
-      else if (i == fruity && j == fruitx)
-      {
-        cout << FGRN("*");
-      }
-      else
-      {
-        bool print = false;
-        for (int k = 0; k < ntail; k++)
+        for(int j=0; j<width;j++)
         {
-          if (tailx[k] == j && taily[k] == i)
-          {
-
-            cout << FWHT("o");
-            print = true;
-          }
+            if(j == 0)
+                cout << FBLU("#");
+            if(i==y && j==x)
+            {
+                cout << FGRN("0");
+            }
+            else if(i == fruity && j == fruitx)
+            {
+                cout << FGRN("*");
+            }
+            else
+            {
+                bool print = false;
+                for(int k =0;k < ntail;k++)
+                {
+                    if(tailx[k] == j && taily[k] ==i)
+                    {
+                        cout<<FWHT("o");
+                        print = true;
+                    }
+                }
+                if(!print)
+                {
+                    cout<<" ";
+                }
+            }
+            if(j==width-1)
+                cout << FRED("|");
         }
-        if (!print)
-        {
-          cout << " ";
-        }
-      }
-      if (j == width - 1)
-        // cout<<"|";
-        cout << FRED("|");
+        cout<<endl;
     }
-    cout << endl;
-  }
-  for (int i = 0; i < width + 2; i++)
-  {
-    cout << FBLU("-");
-  }
-  cout << endl;
-  cout << UNDL(FRED("Score:")) << score << "\t" << endl;
-  cout << FMAG("hi");
+    for(int i = 0;i<width+2;i++)
+        cout << FBLU("-");
+    cout<<endl;
+    cout<< UNDL(FRED("Score:")) <<score<<"\t"<<endl;
 }
-
 void input()
 {
-  if (kbhit())
-  {
-    switch (getch())
+    if (kbhit())
     {
-    case 'a':
-      dir = LEFT;
-      break;
-    case 'd':
-      dir = RIGHT;
-      break;
-    case 'w':
-      dir = UP;
-      break;
-    case 's':
-      dir = DOWN;
-      break;
+        switch (getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        }
     }
-  }
 }
-
 void logic()
 {
-  int prevx = tailx[0];
-  int prevy = taily[0];
-  int prev2x, prev2y;
-  tailx[0] = x;
-  taily[0] = y;
-
-  for (int i = 1; i < ntail; ++i)
-  {
-    prev2x = tailx[i];
-    prev2y = taily[i];
-    tailx[i] = prevx;
-    taily[i] = prevy;
-    prevx = prev2x;
-    prevy = prev2y;
-  }
-
-  switch (dir)
-  {
-  case LEFT:
-    x--;
-    break;
-  case RIGHT:
-    x++;
-    break;
-  case UP:
-    y--;
-    break;
-  case DOWN:
-    y++;
-    break;
-  }
-
-  if (x > width || x < 0 || y > height || y < 0)
-  {
-    gameover = true;
-    cout << "GAME OVER" << endl;
-  }
-
-  for (int i = 0; i < ntail; i++)
-  {
-    if (tailx[i] == x && taily[i] == y)
+    int prevx = tailx[0];
+    int prevy = taily[0];
+    int prev2x, prev2y;
+    tailx[0] = x;
+    taily[0] = y;
+    for(int i=1;i<ntail;i++)
     {
-      gameover = true;
-      cout << "GAME OVER" << endl;
+        prev2x = tailx[i];
+        prev2y = taily[i];
+        tailx[i] = prevx;
+        taily[i] = prevy;
+        prevx = prev2x;
+        prevy = prev2y;
     }
-  }
-
-  if (x == fruitx && y == fruity)
-  {
-    score = score + 1000;
-    fruitx = rand() % width;
-    fruity = rand() % height;
-    ntail++;
-  }
+    switch(dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    }
+    if(x> width || x<0 || y > height || y<0)
+    {
+        gameover=true;
+        cout<<"GAME OVER"<<endl;
+    }
+    for(int i =0; i<ntail;i++)
+    {
+        if(tailx[i]==x && taily[i]==y)
+        {
+            gameover = true;
+            cout<<"GAME OVER"<<endl;
+        }
+    }
+    if(x==fruitx && y==fruity)
+    {
+        score = score +1000;
+        fruitx=rand() % width;
+        fruity=rand() % height;
+        ntail++;
+    }
 }
-
 int main()
 {
-  setup();
-  while (!gameover)
-  {
-    draw();
-    input();
-    logic();
-  }
-  return 0;
+    setup();
+    while(!gameover)
+    {
+        draw();
+        input();
+        logic();
+    }
+    return 0;
 }
-
-
-
-
-
-
